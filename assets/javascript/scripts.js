6320cfe3dabe12539e01bb389b41277bfaa540d8
Cragady <pinkdotdreamer@gmail.com>
2018-05-21 20:28:53 -0600

    added basic layouts and some instructional comments with api key

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,6 @@
+//api key for hw: GhK5NbgIlLn6AVToOnwOLl3clsoaBEdR
+//create buttons for user to press, these buttons will be of different
+//video games
+/*create ajax call for the different buttons, and maybe include
+a custom bar where a user can create a button for a game they want to see gifs
+of and have the button appended and created*/
